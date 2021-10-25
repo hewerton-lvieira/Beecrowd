8859d8f7ffc6c9b46c073845043f69c3ce045d66
Hewerton Vieira <hewerton.lvieira@gmail.com>
2021-10-24 21:59:37 -0300

    1078

---
+++ tabuada/tabuada.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var n int
+	fmt.Scanln(&n)
+	fmt.Printf("1 x %v = %v\n", n, (n * 1))
+	fmt.Printf("2 x %v = %v\n", n, (n * 2))
+	fmt.Printf("3 x %v = %v\n", n, (n * 3))
+	fmt.Printf("4 x %v = %v\n", n, (n * 4))
+	fmt.Printf("5 x %v = %v\n", n, (n * 5))
+	fmt.Printf("6 x %v = %v\n", n, (n * 6))
+	fmt.Printf("7 x %v = %v\n", n, (n * 7))
+	fmt.Printf("8 x %v = %v\n", n, (n * 8))
+	fmt.Printf("9 x %v = %v\n", n, (n * 9))
+	fmt.Printf("10 x %v = %v\n", n, (n * 10))
+}
